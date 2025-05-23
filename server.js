4e408e35951f13686b5f78b5212e67d6ac35505e
EazyGalaxy <84701527+EazyGalaxy@users.noreply.github.com>
2025-05-23 16:38:41 -0400

    Cred Test

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ async function initDb() {
     driver: sqlite3.Database,
   });
 
-  // Create users table (if it doesn't exist) with bettingAmount included. Test
+  // Create users table (if it doesn't exist) with bettingAmount included.
   await db.run(`CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE,
